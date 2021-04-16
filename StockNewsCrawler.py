0b614b23c35cb49ba5bdd56671a8e5cddba55e97
Marlon Braun <marlonb.12.97@gmail.com>
2021-04-16 14:25:56 +0200

    Started coding

--- StockNewsCrawler.py
+++ StockNewsCrawler.py
@@ -1,15 +1,38 @@
 import requests
+import pandas as pd
+import os
+import time
+import random
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+import re
+import numpy as np
+import warnings
+import itertools
 
+option = webdriver.ChromeOptions()
+option.add_argument("--no-sandbox")
+option.add_argument("--disable-dev-shm-usage")
+option.add_argument("--incognito")
+option.add_argument("--disable-infobars")
+option.add_argument("--disable-notifications")
+option.add_experimental_option("excludeSwitches", ["enable-automation"])
 
-names = ["Paulina", "Tom", "Marlon"]
+driver = webdriver.Chrome(options=option)
+driver.implicitly_wait(10)
 
+MostActiveStocks = pd.DataFrame(columns=["Ticker", "Position"])
+url = "https://www.nasdaq.com/market-activity/most-active"
 
-def greet(Nombre):
-    print("Hello" + " " + Nombre)
+driver.get(url)
 
+sourceCode = driver.page_source
+soup = bs(sourceCode, "html.parser")
 
-for name in names:
-    print(greet(name))
+mostActive = soup.find("tbody", {"class": "most-active__body"}).find_all(
+    "tr", class_="most-active__row"
+)
+StockList = []
 
-r = requests.get("https://google.com")
-print(r.status_code)
+for Actives in mostActive:
+    Stock = Actives.find_all("a", class_="firstCell")
