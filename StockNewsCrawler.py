93be8360477e9e684069c0141bb41f7c1b3caa7d
Marlon Braun <marlonb.12.97@gmail.com>
2021-05-04 14:20:57 +0200

    Added code

--- StockNewsCrawler.py
+++ StockNewsCrawler.py
@@ -44,3 +44,58 @@ dfStock = pd.DataFrame(columns=["Ticker", "Name"])
 dfStock["Ticker"] = TickerList
 dfStock["Name"] = NameList
 dfStock.reset_index(drop=True)
+
+dfNews = pd.DataFrame(columns=["ticker", "headline", "text"])
+
+url = "http://www.nasdaq.com/symbol/" + "nflx" + "/news-headlines"
+"""scrape the html of the site"""
+resp = requests.get(links_site)
+
+if not resp.ok:
+    return None
+
+html = resp.content
+
+"""convert html to BeautifulSoup object"""
+soup = BeautifulSoup(html, "lxml")
+
+"""get list of all links on webpage"""
+links = soup.find_all("a")
+
+urls = [link.get("href") for link in links]
+urls = [url for url in urls if url is not None]
+
+"""Filter the list of urls to just the news articles"""
+news_urls = [url for url in urls if "/article/" in url]
+
+
+landing_site = "http://www.nasdaq.com/symbol/" + "nflx" + "/news-headlines"
+
+all_news_urls = get_news_urls(landing_site)
+
+current_urls_list = all_news_urls.copy()
+
+index = 2
+
+"""Loop through each sequential page, scraping the links from each"""
+while (
+    (current_urls_list is not None)
+    and (current_urls_list != [])
+    and (index <= upper_page_limit)
+):
+
+    """Construct URL for page in loop based off index"""
+    current_site = landing_site + "?page=" + str(index)
+    current_urls_list = get_news_urls(current_site)
+
+    """Append current webpage's list of urls to all_news_urls"""
+    all_news_urls = all_news_urls + current_urls_list
+
+    index = index + 1
+
+all_news_urls = list(set(all_news_urls))
+
+"""Now, we have a list of urls, we need to actually scrape the text"""
+# all_articles = [scrape_news_text(news_url) for news_url in all_news_urls]
+
+# return all_articles
